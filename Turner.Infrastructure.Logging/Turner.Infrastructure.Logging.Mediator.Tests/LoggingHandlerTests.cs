b2e3534ccf6c89c7f8fe94b5fc156718e30ab490
Donnell Jenkins <djenkins@donthedev.com>
2019-02-25 10:42:43 -0600

    tests

---
+++ Turner.Infrastructure.Logging/Turner.Infrastructure.Logging.Mediator.Tests/LoggingHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using Turner.Infrastructure.Mediator;
+
+namespace Turner.Infrastructure.Logging.Mediator.Tests
+{
+    [TestFixture]
+    public class LoggingHandlerTests
+    {
+        [Test]
+        public void HandleAsync_XX_XX()
+        {
+            var handlerMock = new Mock<IRequestHandler<TestRequest>>();
+            handlerMock.Setup(x => x.HandleAsync(It.IsAny<TestRequest>()));
+
+            var request = new TestRequest();
+
+            var response = handlerMock.Object.HandleAsync(request);
+
+
+        }
+    }
+
+    class TestRequest : IRequest
+    {
+
+    }
+}
